c6adc23fa2c5c1e431b12a5da1a4e9d60b9db4d1
Bibbe <180316166+Bibbee@users.noreply.github.com>
2025-03-31 12:24:06 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 from bottle import route, run, template, request, static_file 
 import requests
 
-API_KEY = '' # 1. fyll i din unika API-nyckel här. 
+API_KEY = '479dd170c8464b06bc4d46f58ae6cdf7' # 1. fyll i din unika API-nyckel här. 
              # 2. Skaffa gratis nyckel genom att skapa ett konto på:
              # 3. https://spoonacular.com/food-api
              # 4. gör inte för många anrop, du har ca 150pts/dag (gratisplan)
@@ -24,9 +24,10 @@ def get_recipes():
     url = 'https://api.spoonacular.com/recipes/findByIngredients'
     params = {
         'ingredients': ingredients,
-        'number': 5,
+        'number': 20,
         'apiKey': API_KEY
     }
+    
     """parms är en dictionary i python som innehåller alla frågeparametrar
     som skickas med i ett API-anrop via URL:en"""
 
